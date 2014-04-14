8557c4a1c5641290953cf2dc2aabd1e45bff56bb
ianhenderson <ianstein16@gmail.com>
2014-04-13 19:04:37 -0700

    Can (kind of) parse strings, numbers and booleans. Still having trouble with arrays/objects

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,71 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+  var result = [];
+  var temp = "";
+  for(var index = 0; index < json.length; index++){
+    
+    if(json[index] === ","){
+      if(temp === "false"){
+        result.push(false);
+        temp = "";
+        continue;
+      }
+      if(temp === "true"){
+        result.push(true);
+        temp = "";
+        continue;
+      }
+      if(temp === "null"){
+        result.push(null);
+        temp = "";
+        continue;
+      }
+      if(temp.match(/[^\d\.]/) === null){
+        result.push(parseFloat(temp));
+        temp = "";
+        continue;
+      }
+      if(temp === "false"){
+        result.push(false);
+        temp = "";
+        continue;
+      }
+      if(temp === "false"){
+        result.push(false);
+        temp = "";
+        continue;
+      }
+      else{
+        result.push(temp); 
+        temp = "";
+        continue;
+      }
+    }
+    
+    if(json[index] === "["){
+      result.push(parseJSON(json.slice(index+1)));
+      // continue;
+    }
+    if(json[index] === "]"){
+      return result;
+    }
+    
+    if(json[index] === '"'){
+      if(temp.length !== 0){
+        result.push(temp);
+        temp = "";
+        continue;
+      }
+      else{
+        continue;
+      }
+    }
+    if(json[index] === "{"){}
+    
+    if(json[index] === "}"){}
+    
+    else{temp += json[index];}
+  }
+  return result;
 };
